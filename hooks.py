da651a84ad20af08946c8885f481db49fd2d305f
Mehdi Khechine <khechine.mehdi@gmail.com>
2026-01-19 08:24:59 +0100

    feat: Add Repair Management Settings, Dashboard, and .env configuration

---
+++ hooks.py
@@ -0,0 +1,18 @@
+app_name = "repair_management_platform"
+app_title = "Repair Management Platform"
+app_publisher = "ERPBox"
+app_description = "Modular Multi-Industry Repair Management Platform"
+app_email = "contact@erpbox.tn"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/repair_management_platform/css/repair_management_platform.css"
+# app_include_js = "/assets/repair_management_platform/js/repair_management_platform.js"
+
+fixtures = [
+    {"doctype": "Industry Type"},
+    {"doctype": "Diagnostic Checklist Template"}
+]
---
+++ repair_management_platform/repair_management_platform/repair_management_platform/doctype/repair_management_settings/repair_management_settings.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Repair Management Settings', {
+    // refresh: function(frm) {
+    // }
+});
